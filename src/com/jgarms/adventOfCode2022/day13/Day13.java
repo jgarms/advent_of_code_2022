7928fb38ff36ddf1ea318d4804127d48ce620c17
jgarms <jgarms@gmail.com>
2022-12-13 15:05:34 -0800

    Day 13 part two

--- src/com/jgarms/adventOfCode2022/day13/Day13.java
+++ src/com/jgarms/adventOfCode2022/day13/Day13.java
@@ -2,6 +2,9 @@ package com.jgarms.adventOfCode2022.day13;
 
 import com.jgarms.adventOfCode2022.Utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Day13 {
@@ -9,12 +12,19 @@ public class Day13 {
     public static void main(String... args) {
         Day13 day13 = new Day13();
         day13.partOne();
+        day13.partTwo();
     }
 
     private void partOne() {
         Scanner s = Utils.getScanner(this, "day13.txt");
         int sum = sumOrderedPacketIndices(s);
         System.out.println("Part one: " + sum);
+    }
+
+    private void partTwo() {
+        Scanner s = Utils.getScanner(this, "day13.txt");
+        int decoderKey = computeDecoderKey(s);
+        System.out.println("Part two: " + decoderKey);
     }
 
     public static int sumOrderedPacketIndices(Scanner s) {
@@ -34,5 +44,33 @@ public class Day13 {
             index++;
         }
         return sum;
+    }
+
+    public static int computeDecoderKey(Scanner s) {
+        List<ListPacket> packets = new ArrayList<>();
+        while (s.hasNext()) {
+            String line = s.nextLine();
+            if (line.length() > 0) {
+                packets.add(Parser.parse(line));
+            }
+        }
+        ListPacket divider1 = Parser.parse("[[2]]");
+        packets.add(divider1);
+        ListPacket divider2 = Parser.parse("[[6]]");
+        packets.add(divider2);
+        Collections.sort(packets);
+        int divider1Index = 0;
+        int index = 1;
+        for (ListPacket packet : packets) {
+            if (divider1.compareTo(packet) == 0) {
+                divider1Index = index;
+            }
+            else if (divider2.compareTo(packet) == 0) {
+                assert divider1Index > 0;
+                return index * divider1Index;
+            }
+            index++;
+        }
+        throw new IllegalStateException("Internal error: divider packets not found");
     }
 }
