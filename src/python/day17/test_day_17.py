2bd3a8f5725a6d545d2f62693928cd704917accd
jgarms <jgarms@gmail.com>
2022-12-18 15:07:01 -1000

    Day 17 part one in python

--- src/python/day17/test_day_17.py
+++ src/python/day17/test_day_17.py
@@ -3,14 +3,12 @@ import day17
 
 
 class MyTestCase(unittest.TestCase):
-    def test_sample(self):
+    def test_part_one_sample(self):
         jet_pattern = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
         chamber = day17.Chamber(jet_pattern)
         while chamber.num_rocks_landed < 2022:
             chamber.tick()
-        print(chamber.num_rocks_landed)
-        self.assertTrue(True)
-        # self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(3068, chamber.num_rocks_landed)
 
 
 if __name__ == '__main__':
