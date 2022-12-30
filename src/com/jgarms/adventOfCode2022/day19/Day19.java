a45cbf5e9c5004ca6cc98c363eccadf34146cd84
jgarms <jgarms@gmail.com>
2022-12-30 09:43:45 -0800

    Day 19 cleanup

--- src/com/jgarms/adventOfCode2022/day19/Day19.java
+++ src/com/jgarms/adventOfCode2022/day19/Day19.java
@@ -15,33 +15,23 @@ public class Day19 {
     private void partOne() {
         Scanner scanner = Utils.getScanner(this, "day19.txt");
         int total = 0;
-        long start = System.currentTimeMillis();
         while (scanner.hasNext()) {
             Blueprint blueprint = new Blueprint(scanner.nextLine());
-            long blueprintStart = System.currentTimeMillis();
             int max = new State(blueprint, 24).getMaxGeodes();
-            long blueprintTime = System.currentTimeMillis() - blueprintStart;
             int score = max * blueprint.idNumber;
-            System.out.println("Blueprint " + blueprint.idNumber + ": " + score + ". Took " + blueprintTime + "ms.");
             total += score;
         }
-        long time = System.currentTimeMillis() - start;
-        System.out.println("Part one: " + total + ". Took " + time + "ms.");
+        System.out.println("Part one: " + total);
     }
 
     private void partTwo() {
         Scanner scanner = Utils.getScanner(this, "day19.txt");
         int score = 1;
-        long start = System.currentTimeMillis();
         for (int i=0; i<3; i++) {
             Blueprint blueprint = new Blueprint(scanner.nextLine());
-            long blueprintStart = System.currentTimeMillis();
             int max = new State(blueprint, 32).getMaxGeodes();
             score = score * max;
-            long blueprintTime = System.currentTimeMillis() - blueprintStart;
-            System.out.println("Blueprint " + blueprint.idNumber + ": " + max + ". Took " + blueprintTime + "ms.");
         }
-        long time = System.currentTimeMillis() - start;
-        System.out.println("Part two: " + score + ". Took " + time + "ms.");
+        System.out.println("Part two: " + score);
     }
 }
